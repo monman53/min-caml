ee7dff26d7d3d0133fb0da8f1beaf1308848acaf
monman53 <monman.cs@gmail.com>
2019-01-15 11:57:14 +0900

    rewrite README

---
+++ README.ml
@@ -0,0 +1,8 @@
+# FP: Final Report
+
+## Compiler Construction Course
+
+I implemented following enhancements for the purpose of completion of arithmetic operators.
+
+* Integer multiplication and division
+** Constant folding is also implemented.
